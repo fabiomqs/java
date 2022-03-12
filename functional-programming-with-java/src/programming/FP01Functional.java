3af4fd14db88042161cd63b863d322518764a499
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-12 09:56:43 -0300

    change the reference method to use directly from the classSystem.out

--- functional-programming-with-java/src/programming/FP01Functional.java
+++ functional-programming-with-java/src/programming/FP01Functional.java
@@ -14,12 +14,15 @@ public class FP01Functional {
 		// What to do?
 		//convert the list into a stream of numbers
 		numbers.stream()
-				.forEach(FP01Functional::print);// Method Reference
+		//for each on stream of numbers passing method reference
+		//		.forEach(FP01Functional::print);// Method Reference
+		//change the reference method to use directly from the classSystem.out
+				.forEach(System.out::println);// Method Reference
 
 	}
 
-	private static void print(int number) {
-		System.out.println(number);
-	}
+	//private static void print(int number) {
+	//	System.out.println(number);
+	//}
 
 }
