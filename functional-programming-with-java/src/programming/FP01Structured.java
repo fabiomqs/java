110d84c3a51bb942dca1d16e3ddc99c995ee300d
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-12 10:02:53 -0300

    print only even numbers

--- functional-programming-with-java/src/programming/FP01Structured.java
+++ functional-programming-with-java/src/programming/FP01Structured.java
@@ -8,8 +8,10 @@ public class FP01Structured {
 	public static void main(String[] args) {
 
 		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-
-		printAllNumbersInListStructured(numbers);
+		//print all numbers
+		//printAllNumbersInListStructured(numbers);
+		//print only even numbers
+		printEvenNumbersInListStructured(numbers);
 
 	}
 
@@ -18,6 +20,16 @@ public class FP01Structured {
 		for (int number : numbers) {
 			System.out.println(number);
 		}
+	}
+
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+		// How to loop the numbers?
+		for (int number : numbers) {
+			if (number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
+
 	}
 
 }
