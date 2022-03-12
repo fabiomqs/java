f3cefbefe68e0cc440f1fa73fad872b61deb8d70
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-12 10:42:35 -0300

    Exercise 1

--- functional-programming-with-java/src/programming/FP01Exercises.java
+++ functional-programming-with-java/src/programming/FP01Exercises.java
@@ -6,7 +6,15 @@ public class FP01Exercises {
 	
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		//Print Only Odd Numbers from the List
+		printOddNumbersInListFunctional(numbers);
+	}
 
+	// number -> number%2 != 0
+	private static void printOddNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream() // Convert to Stream
+				.filter(number -> number % 2 != 0) // Lamdba Expression
+				.forEach(System.out::println);// Method Reference
 	}
 
 }
