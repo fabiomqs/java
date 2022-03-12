2c975d420bc43d2584a8e67873c24c664d279d59
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-12 10:46:15 -0300

    Exercise 2

--- functional-programming-with-java/src/programming/FP01Exercises.java
+++ functional-programming-with-java/src/programming/FP01Exercises.java
@@ -6,8 +6,13 @@ public class FP01Exercises {
 	
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		List<String> courses =
+				List.of("Spring", "Spring Boot", "API" , "Microservices",
+						"AWS", "PCF","Azure", "Docker", "Kubernetes");
 		//Print Only Odd Numbers from the List
-		printOddNumbersInListFunctional(numbers);
+		//printOddNumbersInListFunctional(numbers);
+		//Print All Courses individually
+		courses.stream().forEach(System.out::println);
 	}
 
 	// number -> number%2 != 0
