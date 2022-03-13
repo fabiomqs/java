85716cef73a79db54f6443337c0579ad28f107a2
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-13 08:32:37 -0300

    writing the complete expressions.

--- functional-programming-with-java/src/programming/FP03FunctionalInterfaces.java
+++ functional-programming-with-java/src/programming/FP03FunctionalInterfaces.java
@@ -25,16 +25,37 @@ public class FP03FunctionalInterfaces {
 		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 
 		Predicate<Integer> isEvenPredicate = x -> x%2==0;
+		//corresponding Predicate
+		Predicate<Integer> isEvenPredicate2 = new Predicate<Integer>() {
+			@Override
+			public boolean test(Integer x) {
+				return x%2==0;
+			}
+
+		};
+
 		Function<Integer, Integer> squareFunction = x -> x * x;
+		//corresponding Function
+		Function<Integer, Integer> squareFunction2 = new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer x) {
+				return x*x;
+			}
+
+		};
+
 		Consumer<Integer> sysoutConsumer = System.out::println;
+		//corresponding Consumer
+		Consumer<Integer> sysoutConsumer2 = new Consumer<Integer>() {
+			public void accept(Integer x) {
+				System.out.println(x);
+			}
+		};
 
 		numbers.stream()
-				.filter(isEvenPredicate)
-				.map(squareFunction)
-				.forEach(sysoutConsumer);
-
-
-
+				.filter(isEvenPredicate2)
+				.map(squareFunction2)
+				.forEach(sysoutConsumer2);
 
 		//numbers.stream()
 					//comment 01 - lambda expression represent a method
