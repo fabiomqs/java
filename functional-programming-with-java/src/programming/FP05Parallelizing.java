0cc397182ae6aa35ec4b7d4df0b2742ec5b332c9
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-15 06:12:09 -0300

    Improving Performance with Parallelization of Streams

---
+++ functional-programming-with-java/src/programming/FP05Parallelizing.java
@@ -0,0 +1,21 @@
+package programming;
+
+import java.util.stream.LongStream;
+
+public class FP05Parallelizing {
+	
+	public static void main(String[] args) {
+		long time = System.currentTimeMillis();
+
+		//0, 1000000000 869
+		System.out.println(LongStream.range(0,1000000000).sum());//499999999500000000
+		System.out.println("the normal operation took: " + (System.currentTimeMillis() - time) + "ms");
+
+		time = System.currentTimeMillis();
+		//601
+		System.out.println(LongStream.range(0,1000000000).parallel().sum());
+
+		System.out.println("the parallel operation took: " + (System.currentTimeMillis() - time) + "ms");
+	}
+
+}
