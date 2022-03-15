4c16b4817ec9c9bef15061ea27d807dd148f2829
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-15 06:26:13 -0300

    Playing with Files using Functional Programming

--- functional-programming-with-java/src/programming/FP05Files.java
+++ functional-programming-with-java/src/programming/FP05Files.java
@@ -8,14 +8,19 @@ import java.util.Arrays;
 public class FP05Files {
 
 	public static void main(String[] args) throws IOException {
-		
-		// Files.lines(Paths.get("file.txt"))
-		// .map(str -> str.split(" "))
-		// .flatMap(Arrays::stream)
-		// .distinct()
-		// .sorted()
-		// .forEach(System.out::println);
-		
+//		read the lines of the file
+//		Files.lines(Paths.get("file.txt"))
+//				.forEach(System.out::println);
+
+//		print all the distinct words in the file
+//		Files.lines(Paths.get("file.txt"))
+//				.map(str -> str.split(" "))
+//				.flatMap(Arrays::stream)
+//				.distinct()
+//				.sorted()
+//				.forEach(System.out::println);
+
+//		list all the directories
 		Files.list(Paths.get("."))
 			.filter(Files::isDirectory)
 			.forEach(System.out::println);
